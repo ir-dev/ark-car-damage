9bf89d0e3385267ae1356653ad713da085c6d3bd
Immanuel R <raj@immanuel.co>
2025-05-24 23:49:17 +0530

    more chagnes.

--- Ark.CarDamage.Detect/Ark.CarDamage.Detect.v1.Web/Api/PredctController.cs
+++ Ark.CarDamage.Detect/Ark.CarDamage.Detect.v1.Web/Api/PredctController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SixLabors.Fonts;
 
 namespace Ark.CarDamage.Detect.v1.Web.Api
 {
@@ -7,5 +8,33 @@ namespace Ark.CarDamage.Detect.v1.Web.Api
     [ApiController]
     public class PredctController : ControllerBase
     {
+        [HttpPost]
+        [Route("v1/cd")]
+        public async Task<dynamic> Index()
+        {
+            if (Request.Form.Files.Count == 0) return new { error = true, msg = "no file found" };
+            try
+            {
+                var fn = Ark.CarDamage.Detect.v1.ArkYoloManager.unique_file_name() + System.IO.Path.GetExtension(Request.Form.Files[0].FileName);
+                var fd = System.IO.Path.Combine(System.Environment.CurrentDirectory, "wwwroot", "detect");
+                if (!Directory.Exists(fd)) Directory.CreateDirectory(fd);
+                var ffn = System.IO.Path.Combine(fd, fn);
+                using (FileStream fss = new FileStream(ffn, FileMode.Create))
+                    Request.Form.Files[0].CopyTo(fss);
+                var res = await Ark.CarDamage.Detect.v1.ArkYoloManager.Predict(ffn);
+                return new
+                {
+                    data = res,
+                    url = $"/detect/segment/{fn}",
+                    error = false,
+                    msg = "predicted success"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new { error = true, msg = $"{ex.Message}" };
+            }
+        }
     }
+
 }
--- Ark.CarDamage.Detect/Ark.CarDamage.Detect.v1.Web/Code/ArkYoloManager.cs
+++ Ark.CarDamage.Detect/Ark.CarDamage.Detect.v1.Web/Code/ArkYoloManager.cs
@@ -5,10 +5,12 @@ namespace Ark.CarDamage.Detect.v1
 {
     public class ArkYoloManager
     {
+        public static Func<string> unique_file_name = () => $"{ark.net.util.DateUtil.CurrentTimeStamp()}_{ark.net.util.ArkHelper.RandomString(8, true)}";
         public static async Task<dynamic> Predict(string img_path)
         {
-            using var predictor = new YoloPredictor(@"C:\Immi\NTT\Hybris_Intern\car_scratch_v2.onnx");
-            return await predictor.PredictAndSaveAsync(@"C:\Immi\NTT\Hybris_Intern\test_7_resize.jpg");
+            using var predictor = new YoloPredictor(System.IO.Path.Combine(System.Environment.CurrentDirectory, "Models", "car_scratch_v2.onnx"));
+            //return await predictor.PredictAndSaveAsync(@"C:\Immi\NTT\Hybris_Intern\test_7_resize.jpg");
+            return await predictor.PredictAndSaveAsync(img_path);
         }
     }
 }
